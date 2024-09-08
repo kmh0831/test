c301a81d06dfeca80f9ba93601d43f8c067de150
kmh0831 <kmhyuk1018@gmail.com>
2024-09-08 23:25:25 +0900

    first commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // 'Switch' 대신 'Routes' 사용
 import MovieList from './components/MovieList';
 import Login from './components/Login';
 import Signup from './components/Signup';  // Signup 컴포넌트 추가
@@ -11,19 +11,19 @@ function App() {
     <Router>
       <div className="App">
         <h1>OTT Movie Service</h1>
-        <Switch>
+        <Routes> {/* 'Switch' 대신 'Routes'로 변경 */}
           {/* 기본 경로: 로그인 페이지 */}
-          <Route exact path="/" component={Login} />
+          <Route path="/" element={<Login />} />
           
           {/* 영화 목록 페이지: 로그인 여부와 상관없이 접근 가능 */}
-          <Route path="/movies" component={MovieList} />
+          <Route path="/movies" element={<MovieList />} />
 
           {/* 회원가입 페이지 */}
-          <Route path="/signup" component={Signup} />
+          <Route path="/signup" element={<Signup />} />
           
           {/* 찜한 영화 목록 페이지: 로그인한 사용자만 접근 가능 */}
-          <Route path="/favorites" component={FavoriteMovies} />
-        </Switch>
+          <Route path="/favorites" element={<FavoriteMovies />} />
+        </Routes>
       </div>
     </Router>
   );
